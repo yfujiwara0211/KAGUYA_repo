d623ec0bb631dd85a4abe538ccf63b8d9096685f
yfujiwara0211 <50650782+yfujiwara0211@users.noreply.github.com>
2024-04-17 22:00:17 +0900

    Update page.jsx

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -23,13 +23,7 @@ const Register = () => {
 
     const handleChange = (e) => {
         const { name, value } = e.target;
-        // industryフィールドの値を設定する際に、IndustrySelectBox配列の各オブジェクトのvalueプロパティを使用する
-        if (name === 'industry') {
-            const selectedIndustry = IndustrySelectBox.find(item => item.value === value);
-            setUser({ ...user, [name]: selectedIndustry ? selectedIndustry.value : value });
-        } else {
-            setUser({ ...user, [name]: value });
-        }
+        setUser({ ...user, [name]: value });
     };
 
     const register = async (userInfo) => {
